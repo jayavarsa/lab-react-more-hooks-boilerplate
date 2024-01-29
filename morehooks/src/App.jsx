e794190c5ed5a2c83301dcfa30895ba474f54154
jayavarsan <jayavarsan.r@kalvium.community>
2024-01-29 10:17:49 +0530

    code

---
+++ morehooks/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import Tasklist from './Components/Tasklist'
+
+function App() {
+
+  return (
+    <>
+      < Tasklist />
+    </>
+  )
+}
+
+export default App
